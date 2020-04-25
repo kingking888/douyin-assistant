3389794a798ed1983e14c5a68fd012051d10f6a0
liuliang <satng@qq.com>
2020-04-25 18:13:11 +0800

    fix

--- app/src/main/java/vip/mimiya/helper/BaseApplication.java
+++ app/src/main/java/vip/mimiya/helper/BaseApplication.java
@@ -15,14 +15,50 @@ import com.yhao.floatwindow.PermissionListener;
 import com.yhao.floatwindow.Screen;
 import com.yhao.floatwindow.ViewStateListener;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class BaseApplication extends Application {
     private static final String TAG = "BaseApplication";
-    private ClipboardManager clipboard;
+    private ClipboardManager clipboardManager;
+    private boolean status = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        clipboard = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboardManager = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboardManager.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
+            @Override
+            public void onPrimaryClipChanged() {
+                ClipData clipData = clipboardManager.getPrimaryClip();
+                String pasteString = "";
+                if (clipData != null && clipData.getItemCount() > 0) {
+                    CharSequence text = clipData.getItemAt(0).getText();
+                    pasteString = text.toString();
+                }
+                if (!status) return;
+                if (TextUtils.isEmpty(pasteString)) return;
+
+                String pattern = "(https://v.douyin.com).*/";
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(pasteString);
+                String shareUrl = null;
+                if (m.find()) {
+                    shareUrl = m.group(0);
+                    postShareUrl(shareUrl);
+                    Toast.makeText(BaseApplication.this, "收到任务:[" + pasteString + "]", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(BaseApplication.this, "NO MATCH!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         ImageView imageView = new ImageView(getApplicationContext());
         imageView.setImageResource(R.drawable.icon);
 
@@ -41,20 +77,46 @@ public class BaseApplication extends Application {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ClipData clipData = clipboard.getPrimaryClip();
-                String pasteString = "";
-                if (clipData != null && clipData.getItemCount() > 0) {
-                    CharSequence text = clipData.getItemAt(0).getText();
-                    pasteString = text.toString();
-                }
-                if (TextUtils.isEmpty(pasteString)) return;
-                Toast.makeText(BaseApplication.this, "收到任务:[" + pasteString + "]", Toast.LENGTH_LONG).show();
+                if (status) ((ImageView) v).setImageResource(R.drawable.icon);
+                else ((ImageView) v).setImageResource(R.drawable.icon1);
+                status = !status;
+//                ClipData clipData = clipboardManager.getPrimaryClip();
+//                String pasteString = "";
+//                if (clipData != null && clipData.getItemCount() > 0) {
+//                    CharSequence text = clipData.getItemAt(0).getText();
+//                    pasteString = text.toString();
+//                }
+//                if (TextUtils.isEmpty(pasteString)) return;
+//                Toast.makeText(BaseApplication.this, "收到任务:[" + pasteString + "]", Toast.LENGTH_LONG).show();
 
                 //TODO POST
             }
         });
     }
 
+    private void postShareUrl(final String url) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                MediaType JSON = MediaType.get("application/json; charset=utf-8");
+                OkHttpClient client = new OkHttpClient();
+                String json = String.format("{\"url\":\"%s\"}", url);
+                RequestBody body = RequestBody.create(json, JSON);
+                Request request = new Request.Builder()
+                        .url("http://192.168.1.4:5015/api/caches/video/clone/123467890")
+                        .post(body)
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    System.out.println(response.body().string());
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }).start();
+
+    }
 
     private PermissionListener mPermissionListener = new PermissionListener() {
         @Override
