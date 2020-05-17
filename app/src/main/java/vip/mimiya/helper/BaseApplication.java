7e33480a78973bb46ca155d383fc3dd602a464d9
liuliang <satng@qq.com>
2020-05-17 09:47:19 +0800

    fix

--- app/src/main/java/vip/mimiya/helper/BaseApplication.java
+++ app/src/main/java/vip/mimiya/helper/BaseApplication.java
@@ -4,11 +4,19 @@ import android.app.Application;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NavUtils;
 
 import com.yhao.floatwindow.FloatWindow;
 import com.yhao.floatwindow.PermissionListener;
@@ -29,6 +37,7 @@ public class BaseApplication extends Application {
     private static final String TAG = "BaseApplication";
     private ClipboardManager clipboardManager;
     private boolean status = false;
+    private String lastPasteString;
 
     @Override
     public void onCreate() {
@@ -59,16 +68,62 @@ public class BaseApplication extends Application {
                 }
             }
         });
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View floatLayout = inflater.inflate(R.layout.input_layout, null);
+
+        final EditText postEditText = floatLayout.findViewById(R.id.post_editText);
+        Button postButton = floatLayout.findViewById(R.id.post_button);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sharp_url = postEditText.getText().toString();
+                postEditText.setText("");
+
+                postEditText.findFocus();
+
+                ClipData clipData = clipboardManager.getPrimaryClip();
+                String pasteString = "";
+                if (clipData != null && clipData.getItemCount() > 0) {
+                    CharSequence text = clipData.getItemAt(0).getText();
+                    pasteString = text.toString();
+                }
+                // if (!status) return;
+                if (TextUtils.isEmpty(pasteString)) return;
+
+                clipboardManager.setPrimaryClip(ClipData.newPlainText("aaa", "bbb"));
+
+                if (!TextUtils.isEmpty(lastPasteString) && lastPasteString.equals(pasteString))
+                    return;
+
+
+                lastPasteString = pasteString;
+
+                String pattern = "(https://v.douyin.com).*/";
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(pasteString);
+
+                String shareUrl = null;
+                if (m.find()) {
+                    shareUrl = m.group(0);
+                    postShareUrl(shareUrl);
+                    Toast.makeText(BaseApplication.this, "收到任务:[" + pasteString + "]", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(BaseApplication.this, "NO MATCH!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         ImageView imageView = new ImageView(getApplicationContext());
         imageView.setImageResource(R.drawable.icon);
 
         FloatWindow
                 .with(getApplicationContext())
-                .setView(imageView)
-                .setWidth(150)                               //设置控件宽高
+                .setView(floatLayout)
+                //.setWidth(150)                               //设置控件宽高
                 .setHeight(Screen.width, 0.2f)
                 .setX(0)                                   //设置控件初始位置
-                .setY(Screen.height, 0.3f)
+                .setY(Screen.height, 0.1f)
                 .setDesktopShow(true)                        //桌面显示
                 .setViewStateListener(mViewStateListener)    //监听悬浮控件状态改变
                 .setPermissionListener(mPermissionListener)  //监听权限申请结果
@@ -80,6 +135,36 @@ public class BaseApplication extends Application {
                 if (status) ((ImageView) v).setImageResource(R.drawable.icon);
                 else ((ImageView) v).setImageResource(R.drawable.icon1);
                 status = !status;
+
+                ClipData clipData = clipboardManager.getPrimaryClip();
+                String pasteString = "";
+                if (clipData != null && clipData.getItemCount() > 0) {
+                    CharSequence text = clipData.getItemAt(0).getText();
+                    pasteString = text.toString();
+                }
+                // if (!status) return;
+                if (TextUtils.isEmpty(pasteString)) return;
+
+                clipboardManager.setPrimaryClip(ClipData.newPlainText("aaa", "bbb"));
+
+                if (!TextUtils.isEmpty(lastPasteString) && lastPasteString.equals(pasteString))
+                    return;
+
+
+                lastPasteString = pasteString;
+
+                String pattern = "(https://v.douyin.com).*/";
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(pasteString);
+
+                String shareUrl = null;
+                if (m.find()) {
+                    shareUrl = m.group(0);
+                    postShareUrl(shareUrl);
+                    Toast.makeText(BaseApplication.this, "收到任务:[" + pasteString + "]", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(BaseApplication.this, "NO MATCH!", Toast.LENGTH_SHORT).show();
+                }
 //                ClipData clipData = clipboardManager.getPrimaryClip();
 //                String pasteString = "";
 //                if (clipData != null && clipData.getItemCount() > 0) {
@@ -104,20 +189,40 @@ public class BaseApplication extends Application {
                 String json = String.format("{\"url\":\"%s\"}", url);
                 RequestBody body = RequestBody.create(json, JSON);
                 Request request = new Request.Builder()
-                        .url("http://192.168.1.4:5015/api/caches/video/clone/123467890")
+                        .url("http://27.115.87.106:5015/api/caches/video/clone/" + String.valueOf(System.currentTimeMillis()))
                         .post(body)
                         .build();
+                Message msg = handler.obtainMessage(100);
                 try {
                     Response response = client.newCall(request).execute();
-                    System.out.println(response.body().string());
+                    String result = response.body().string();
+                    System.out.println(result);
+                    msg.obj = result;
+                    handler.sendMessage(msg);
+
                 } catch (IOException e) {
-                    System.out.println(e.getMessage());
+                    msg.obj = e.getMessage();
+                    handler.sendMessage(msg);
                 }
             }
         }).start();
 
     }
 
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 100:
+                    String result = (String) msg.obj;
+                    Toast.makeText(BaseApplication.this, result, Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
     private PermissionListener mPermissionListener = new PermissionListener() {
         @Override
         public void onSuccess() {
